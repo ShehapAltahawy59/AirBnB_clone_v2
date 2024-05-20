6b99d470efa4ade2d18e124b4af3b75b0723cb65
shehap62 <shehapkhalil62@gmail.com>
2024-05-20 21:05:10 +0300

    edit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,7 +6,7 @@ A script that starts a Flask web application:
 from flask import Flask
 
 app =Flask(__name__)
-@app.route("/")
+@app.route("/",strict_slashes=False)
 def hello():
     """
     Displays 'Hello HBNB!'
